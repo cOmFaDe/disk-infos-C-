20c198272bdffaa4dbf8e200113251479917cfc3
Vitor Luiz <vitorlemeslemes6@gmail.com>
2023-02-22 23:04:49 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+DriveInfo[] mostrarDrivers = DriveInfo.GetDrives();
+
+
+Console.WriteLine("DISK INFOS");
+
+
+    DriveInfo drive = new DriveInfo(@"C:\");
+    Console.WriteLine("Disk Format: {0}", drive.DriveFormat);
+    Console.WriteLine("Disk Size: {0,15}", drive.TotalSize);
+    Console.WriteLine("Disk Type: {0}", drive.DriveType);
+    Console.WriteLine("Disk Free Space:: {0,15}", drive.AvailableFreeSpace);
+    Console.WriteLine("Disk Root Directory: {0} ",drive.RootDirectory);
+ Console.ReadKey();
+
+
